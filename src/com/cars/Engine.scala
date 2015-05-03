f85ff5fff9a95010e93f9635e6f82671d78eda1a
nsankaran <nsankaran@ebay.com>
2015-05-02 22:56:26 -0700

    added base classes

---
+++ src/com/cars/Engine.scala
@@ -0,0 +1,5 @@
+package com.cars
+
+class Engine {
+
+}
---
+++ src/com/cars/MainProgram.scala
@@ -0,0 +1,30 @@
+package com.cars
+
+import java.util.Date
+
+class Immutable( n: Int){
+  val value = n;
+  override def toString(): String = "("+ value + ")";
+}
+
+class Mutable( x:Int){
+  var value = x;
+  def setValue( n:Int){ value =n; }
+  override def toString(): String = "("+ value + ")";
+}
+
+object MainProgram {
+  def main( args: Array[String] ){
+    
+    val mutable = new Mutable(10);
+    val immutable = new Immutable(5);
+    
+    println("mutable = " + mutable);
+    println("immutable = " + immutable);
+    
+    mutable.value = 100;
+    
+    println("mutable = " + mutable);
+    println("immutable = " + immutable);
+  }
+}
